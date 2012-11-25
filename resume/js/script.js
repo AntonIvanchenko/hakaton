4ce09ca8d331cd1f5d5f37d0e9158013e77bb24e
Kate Arapova <katearapova@gmail.com>
2012-11-25 10:20:13 +0200

    HTML

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,31 +1,76 @@
 $(document).ready(function(){
-    var resum = $('#resume');
+var resum = $('#resume'),
+        caruselFirstNum = 0,
+        caruselFirstAllTemp = $("#inner>ul>li").length,
+        innerFirst = $("#inner>ul");
+        
     $("#templatesMenu>li>a").click(function(){
-        var resumTemplate = $(this).attr('href');
-        console.log(resumTemplate)
+        var button = $(this),
+            resumTemplate = button.attr('href');
         resum.removeClass().addClass(resumTemplate);
+        
+        if(button.parent().hasClass("paid_template")){
+            $("#next").css({"display":"none"});
+            $("#buy").css({"display":"inline-block"}); 
+        }else{
+            $("#next").css({"display":"inline-block"});
+            $("#buy").css({"display":"none"}); 
+        }
         return false;
     });
     // step 2
-    $(".hide-show").click(function(){
-    	$(this).parent(".param").addClass("param-hidden");
-    	return false;
+    $(".hide-show").toggle(
+        function(){
+                $(this).parent().find("p").slideUp(300);
+            $(this).text("show")
+                return false;
+        },
+        function(){
+                $(this).parent().find("p").slideDown(300);
+            $(this).text("hide")
+                return false;
+        }
+    );
+
+    $('#menu').superfish({
+      delay:       500,
+      animation:   {height:'show'},
+      speed:       600,
+      autoArrows:  false,
+      dropShadows: false,
     });
-    $(".hide-show span").click(function(){
-    	$(this).parent().parent(".param").removeClass("param-hidden");
-    	return false;
+    $(".fancyImg").fancybox({
+        'transitionIn'  :   'swing',
+        'transitionOut' :   'swing',
+        'speedIn'       :   600, 
+        'speedOut'      :   200, 
+        'overlayShow'   :   true,
+        'overlayColor'  :   "#000000"
     });
-    // buy btn
-   /* $("#templatesMenu>.paid_template>a").click(function(){
-    	$("#next").addClass("hidden");
-    	$("#buy").removeClass("hidden");
-    	return false;
-    });
-    $("#templatesMenu>.free_template>a").click(function(){
-    	$("#buy").addClass("hidden");
-    	$("#next").removeClass("hidden");
-    	return false;
-    }); */
+    $("#nextTemp").click(function(){
+        if(caruselFirstNum<caruselFirstAllTemp-3){
+            caruselFirstNum++
+            innerFirst.stop(true).animate({"left":-caruselFirstNum*320}, 700, "easeInOutCubic")
+        }
+        return false;
+    })
+    $("#prevTemp").click(function(){
+        if(caruselFirstNum>0){
+            caruselFirstNum--
+            innerFirst.stop(true).animate({"left":-caruselFirstNum*320}, 700, "easeInOutCubic")
+        }
+        return false;
+    })
+    $("#inner a").hover(
+        function(){
+            $("img", this).stop(true).animate({"top":-20}, 500, "easeInOutCubic");
+            $(".imgShadow", this).stop(true).fadeTo(400, 0.5, "easeInOutCubic");
+        },
+        function(){
+            $("img", this).stop(true).animate({"top":0}, 500, "easeInOutCubic");
+            $(".imgShadow", this).stop(true).fadeTo(400, 1, "easeInOutCubic"); 
+        }
+    )
 
     // step 2 / Carousel
     $( '#templatesMenu' ).elastislide();
