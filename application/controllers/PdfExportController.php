615ba33e698367134e07309aa69532e504ca46f3
pyatachok <pyatachok@192.168.150.101>
2012-11-25 10:50:37 +0200

    Контроллер экспорта

---
+++ application/controllers/PdfExportController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+class PdfExportController extends Zend_Controller_Action
+{
+
+
+	public function init ()
+	{
+		/* Initialize action controller here */
+	}
+
+
+	public function indexAction ()
+	{
+//		require_once 'linkedin.php';
+	}
+
+	public function exportAction ()
+	{
+		
+		
+//		$this->_helper->redirector('/');
+	}
+}
+
+
