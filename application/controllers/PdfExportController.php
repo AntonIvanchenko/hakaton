2a2272abddb72ea96ab067896d93f24797cff8fc
pyatachok <pyatachok@192.168.150.101>
2012-11-25 13:24:34 +0200

    Контроллер экспорта

--- application/controllers/PdfExportController.php
+++ application/controllers/PdfExportController.php
@@ -20,9 +20,33 @@ class PdfExportController extends Zend_Controller_Action
 
 	public function exportAction ()
 	{
-		
-		
-//		$this->_helper->redirector('/');
+		// create PDF
+		$pdf = new Zend_Pdf();
+		// create A4 page
+		$page = new Zend_Pdf_Page(Zend_Pdf_Page::SIZE_A4);
+
+		// define font resource
+		$font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA);
+
+		// set font for page
+		// write text to page
+		$page->setFont($font, 24)
+			 ->drawText('That which we call a rose,', 72, 720)
+			 ->drawText('By any other name would smell as sweet.', 72, 620);
+
+		// add page to document
+		$pdf->pages[] = $page;
+		$filename = 'downloads/resume_'.date('Y_m_d_H_i_s',time()).'.pdf'; 
+		$pdf->save($filename);
+
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender();
+
+		$pdf = Zend_Pdf::load($filename);
+
+		$this->getResponse()->setHeader('Content-type', 'application/x-pdf', true);
+		$this->getResponse()->setHeader('Content-disposition', 'inline; filename=your_resume.pdf', true);
+		$this->getResponse()->setBody($pdf->render());
 	}
 }
 
